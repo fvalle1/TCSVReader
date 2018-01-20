7a52a1a4d0ef0b5eb59518c34d506e0b3a41763b
Filippo Valle <filippovalle@aim.com>
2018-01-20 20:28:20 +0100

    use complete filename

--- sample.C
+++ sample.C
@@ -1,7 +1,7 @@
 //call with root -l -e sample.C
 
 void sample(){
-  TCSVReader *reader=new TCSVReader("test");
+  TCSVReader *reader=new TCSVReader("test.csv");
   TGraph *g=reader->GetTGraphErrors();
   g->Draw();
   return;
